190e501923ddcd068e98178199b61c3f591c65cd
Zhliu66 <74619206+Zhliu66@users.noreply.github.com>
2020-11-20 13:46:56 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,2 +1,17 @@
 colour = input("whats your favourie colour?")
 print(colour + "?, thats my favourite colour too! ")
+
+cans = input("How many cans are there per pack?" )
+packs = input("How many packs are there?")
+print("There are" + (int(cans) * int(packs)) + "cans in total")
+
+print("what is the length of x in meters?")
+x = input(int())
+print("what is the length of y in meters?")
+y = input(int())
+print("what is the length of z in meters?")
+z = input(int())
+cube = int(x) * int(y) * int(z)
+print("The volume of the object in cubic meters is")
+print(cube)
+
